8b9fe1b70468a22f1f7d4ef5c826445093f68664
Cathy <cathyludev@gmail.com>
2022-02-17 18:36:57 -0500

    Fixing path error

--- routes/routes.js
+++ routes/routes.js
@@ -8,19 +8,21 @@ router.get('/categories', async(req, res, next) => {
     try {
         const categories = await Category.findAll();
         
-        /*const html = `
+        const html = `
         <html>
             <body>
                 <h1>The Legend of Zelda Item Wiki for Breath of the Wild</h1>
                 <p>On all adventures, both free-roaming and guided, there are items for Link to grab and use in the world. Check out some of them here!</p>
-                <h3>Categories</h3>
+                <h2>Categories</h2>
                 <div>${categories.map(category => {
                     return `<div><a href="/categories/${category.id}">${category.name}</a></div>`;
                 }).join('')}</div>
+                <h3>[JSON info for debugging]</h3>
+                <div>${JSON.stringify(categories)}</div>
             </body>
         </html>
-        `*/
-        res.send(categories);
+        `
+        res.send(html);
     } catch(err) {
         next(err);
     }
@@ -30,9 +32,6 @@ router.get('/categories', async(req, res, next) => {
 router.get('/categories/:id', async(req, res, next) => {
     try {
         const category = await Category.findAll({
-            include: [
-                {model: Category}
-            ]
         });
         const html = `
         <html>
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-const itemsRoutes = require('./routes/items');
+//const itemsRoutes = require('./routes/items');
 
-//app.use('/', routes);
+app.use('/', routes);
 //app.use('/items', itemsRoutes);
-
-/*=app.get('/items', async(req, res) => {
-    //const data = await client.query('');
-    res.send("This is a list of items");
-});*/
 
 const port = 3000;
 const startup = async() => {
